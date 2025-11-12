08d522bebbd05be9eb15d8363f103013eb015a60
Võ Anh Bằng <anhbangluckystar@gmail.com>
2025-11-13 03:33:26 +0700

    Create UserRepository.java

---
+++ backend/src/main/java/com/profile/api/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.profile.api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.profile.api.entity.UserEntity;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+
+    Optional<UserEntity> findByEmail(String email);
+}
