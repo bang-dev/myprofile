595481d9f3022d1c354a6f6beab1dbbff892496d
Võ Anh Bằng <anhbangluckystar@gmail.com>
2025-11-13 08:19:24 +0700

    Update ProfileRequest.java

--- backend/src/main/java/com/profile/api/io/ProfileRequest.java
+++ backend/src/main/java/com/profile/api/io/ProfileRequest.java
@@ -1,5 +1,6 @@
 package com.profile.api.io;
 
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,7 +12,12 @@ import lombok.NoArgsConstructor;
 @Builder
 public class ProfileRequest {
 
+    @NotBlank(message = "Name cannot be blank")
     private String name;
+    @Email(message = "Email should be valid")
+    @NotNull(message = "Email cannot be null")
     private String email;
+    @Min(value = 6, message = "Password must be at least 6 characters long")
+    @Max(value = 10,message = "Password must be at most 10 characters long")
     private String password;
 }
